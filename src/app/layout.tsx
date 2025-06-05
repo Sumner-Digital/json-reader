51df51e941657104284e3ca8d5cda4f175c0540d
[Your Username] <scott@sumnerdigital.com>
2025-06-05 14:06:45 -0400

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>{children}</body>
-    </html>
+    </html >
   );
 }
